dbff0f70ab031f55d98a9155f2517c1cea63687c
kinda_funnyz <nikita_sigarev@mail.ru>
2025-06-30 02:09:41 +0700

    wip

--- src/components/UserTable/DesktopUserTable.vue
+++ src/components/UserTable/DesktopUserTable.vue
@@ -2,7 +2,6 @@
 import { useAccountStore } from '@/stores/accountStore.ts';
 import { onMounted } from 'vue';
 import DesktopAccountForm from '@/components/UserTable/UserForm/DesktopAccountForm.vue';
-import MobileAccountForm from '@/components/UserTable/UserForm/MobileAccountForm.vue';
 const needNew = defineModel<boolean>();
 const store = useAccountStore();
 onMounted(() => {
@@ -20,24 +19,62 @@ onMounted(() => {
       <div class="header-cell"></div>
     </div>
 
-    <div v-for="account in store.accounts" :key="account.id" class="table-row">
+    <div v-for="account in store.accounts" :key="account.id">
       <desktop-account-form
         :value="account"
+        class="table-row"
         @remove="store.removeAccount"
         @saved="store.editAccount"
       />
-      <desktop-account-form
-        v-if="needNew"
-        @saved="
-          (account) => {
-            store.addAccount(account);
-            needNew = false;
-          }
-        "
-        @remove="() => (needNew = false)"
-      />
     </div>
+    <desktop-account-form
+      v-if="needNew"
+      class="table-row"
+      @saved="
+        (account) => {
+          store.addAccount(account);
+          needNew = false;
+        }
+      "
+      @remove="() => (needNew = false)"
+    />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.account-table {
+  width: 100%;
+  border: 1px solid #ebeef5;
+  border-radius: 4px;
+  overflow: hidden;
+}
+
+.table-header,
+.table-row {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 0.3fr;
+  gap: 1px;
+  background-color: #ebeef5;
+}
+
+.header-cell,
+.table-cell {
+  padding: 12px 16px;
+  background-color: white;
+  overflow: hidden;
+  min-width: 46px;
+}
+
+.header-cell {
+  font-weight: bold;
+  color: #333;
+}
+
+.table-row.has-error {
+  background-color: #fff1f0;
+}
+
+.is-error :deep(.el-input__wrapper) {
+  box-shadow: 0 0 0 1px #f56c6c inset;
+}
+</style>
--- src/components/UserTable/UserForm/DesktopAccountForm.vue
+++ src/components/UserTable/UserForm/DesktopAccountForm.vue
@@ -9,9 +9,9 @@ const { formRef, data, rules, updateData } = useAccountForm(props, emit);
 </script>
 
 <template>
-  <el-form :model="data" :rules="rules" ref="formRef" :inline="true">
+  <el-form :model="data" :rules="rules" ref="formRef" label-position="top">
     <div class="table-cell">
-      <el-form-item :error="data.errors.label ? 'Максимум 50 символов' : ''">
+      <el-form-item prop="label">
         <el-tooltip content="Метки разделяются через ;">
           <el-input
             v-model="data.label"
@@ -22,7 +22,7 @@ const { formRef, data, rules, updateData } = useAccountForm(props, emit);
     </div>
 
     <div class="table-cell">
-      <el-form-item>
+      <el-form-item prop="type">
         <el-select
           v-model="data.type"
           @change="(val: RecordType) => updateData()"
@@ -39,32 +39,30 @@ const { formRef, data, rules, updateData } = useAccountForm(props, emit);
       </el-form-item>
     </div>
 
-    <div class="table-cell">
-      <el-form-item
-        :error="
-          data.errors.login ? 'Обязательное поле, максимум 100 символов' : ''
-        "
-      >
+    <div
+      class="table-cell"
+      :class="{ 'merge-next': data.type == RecordType.ldap }"
+    >
+      <el-form-item prop="login">
         <el-input
           v-model="data.login"
+          autocomplete="off"
+          name="dummy-login"
           @blur="() => updateData()"
           placeholder="Логин"
         />
       </el-form-item>
     </div>
 
-    <div class="table-cell">
-      <el-form-item
-        v-if="data.type === RecordType.local"
-        :error="
-          data.errors.password ? 'Обязательное поле, максимум 100 символов' : ''
-        "
-      >
+    <div v-if="data.type === RecordType.local" class="table-cell">
+      <el-form-item prop="password">
         <el-input
           v-model="data.password"
+          autocomplete="new-password"
+          name="dummy-password"
+          @blur="() => updateData()"
           type="password"
           show-password
-          @blur="() => updateData()"
           placeholder="Пароль"
         />
       </el-form-item>
@@ -77,4 +75,68 @@ const { formRef, data, rules, updateData } = useAccountForm(props, emit);
   </el-form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.mb-4 {
+  margin-bottom: 1rem;
+}
+.mb-6 {
+  margin-bottom: 1.5rem;
+}
+.p-4 {
+  padding: 1rem;
+}
+.border {
+  border: 1px solid #dcdfe6;
+}
+.rounded-bd {
+  border-radius: 6px;
+}
+.bg-white {
+  background-color: #ffffff;
+}
+.flex-wrap {
+  flex-wrap: wrap;
+}
+.items-end {
+  align-items: flex-end;
+}
+
+.account-table {
+  width: 100%;
+  border: 1px solid #ebeef5;
+  border-radius: 4px;
+  overflow: hidden;
+}
+
+.table-header,
+.table-row {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 0.3fr;
+  gap: 1px;
+  background-color: #ebeef5;
+}
+
+.header-cell,
+.table-cell {
+  padding: 12px 16px;
+  background-color: white;
+  overflow: hidden;
+  min-width: 46px;
+}
+
+.header-cell {
+  font-weight: bold;
+  color: #333;
+}
+
+.table-row.has-error {
+  background-color: #fff1f0;
+}
+
+.is-error :deep(.el-input__wrapper) {
+  box-shadow: 0 0 0 1px #f56c6c inset;
+}
+.merge-next {
+  grid-column: span 2;
+}
+</style>
